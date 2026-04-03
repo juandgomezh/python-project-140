5e4ff5412f25faae033230ef1e1bf6351ba6d1ff
JD <juandgomezh@outlook.com>
2026-04-03 16:23:10 -0500

    feat: se corrigen los errores por medio del comando uv run ruff check --fix

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
 from brain_games.cli import welcome_user
 
+
 def main():
+    
     welcome_user()
+    
 
 if __name__ == "__main__":
+    
     main()
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def main():
     print("Welcome to my project <JDG>!")
 
+
 from brain_games.scripts.brain_games import main
+
 
 def main():
     main()
+
 
 main()
