75e3c87206236a29ed385af81aa659944b2d664f
JD <juandgomezh@outlook.com>
2026-04-30 21:03:38 -0500

    feat: revisiones finales

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,10 @@
-from brain_games.games.even import DESCRIPTION, play, validate_answer
+from brain_games.core import play
+from brain_games.games.even import DESCRIPTION, get_round_data
 
 
 def main():
     try:
-        play(DESCRIPTION, validate_answer)
+        play(DESCRIPTION, get_round_data)
     except KeyboardInterrupt:
         print("\nExit the even game...")
 
